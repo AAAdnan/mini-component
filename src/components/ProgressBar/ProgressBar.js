063c762bac4f0298ceba8dfccfd8b034992ca0c1
Adnan Ahmed <adnan.ahmed01@gmail.com>
2022-02-01 15:48:12 -0500

    added progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,31 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--borderRadius": 2 + "px",
+    "--fontSize": 16 / 16 + "rem",
+    "--padding": "4px 12px"
+  },
+  medium: {
+    "--borderRadius": 2 + "px",
+    "--fontSize": 18 / 16 + "rem",
+    "--padding": "12px 20px"
+  },
+  large: {
+    "--borderRadius": 4 + "px",
+    "--fontSize": 21 / 16 + "rem",
+    "--padding": "16px 32px"
+  }
+};
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <progress max="100" min="0" value={value} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">{value}</progress>
+  ;
+  
 };
 
 export default ProgressBar;
+
+
+
