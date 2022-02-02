3c62a3acb8c353cdc14951f0d9d30ee59bd67c2c
Adnan Ahmed <adnan.ahmed01@gmail.com>
2022-02-02 16:24:28 -0500

    built progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,35 +1,76 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const SIZES = {
+const STYLES = {
   small: {
-    "--borderRadius": 2 + "px",
-    "--fontSize": 16 / 16 + "rem",
-    "--padding": "4px 12px"
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
   medium: {
-    "--borderRadius": 2 + "px",
-    "--fontSize": 18 / 16 + "rem",
-    "--padding": "12px 20px"
+    height: 12,
+    padding: 0,
+    radius: 4,
   },
   large: {
-    "--borderRadius": 4 + "px",
-    "--fontSize": 21 / 16 + "rem",
-    "--padding": "16px 32px"
-  }
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
 };
 
 const ProgressBar = ({ value, size }) => {
-  return <progress max="100" min="0" value={value} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">{value}</progress>
-  ;
-  
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            '--width': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
+  );
 };
 
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
+
 export default ProgressBar;
-
-
-
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,13 +6,42 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
+
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <PresentationalBits>
+        Hello World { '' }
+        <Icon id="chevron-down" strokeWidth={1} size={24} />
+      </PresentationalBits>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const PresentationalBits = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16/16} rem;
+  padding: 12px 16px;
+`;
+
+
+const StyledSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+ `;
 
 export default Select;
